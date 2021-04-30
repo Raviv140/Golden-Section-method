e4c8ac3594211ba05c18becde5400a6687fccb74
Raviv Herrera <59799288+Raviv140@users.noreply.github.com>
2021-04-30 08:34:37 +0300

    Update Golden_Section.py

--- Golden_Section.py
+++ Golden_Section.py
@@ -36,7 +36,6 @@ def golden_method(f,xx, rng = 1):
             a_new = b_new
             b_new = tail + (1-ro)*(head - tail)
            
-
     return tail , head
   
   
@@ -53,28 +52,10 @@ if __name__ == "__main__":
   
   for i in range(xx.size):
     yy[i] = f.subs(x, xx[i])
-  
-  #plot of the function with point a and point b 
-  plt.figure(figsize=(8, 6))
-  plt.plot(xx, yy, linewidth=2.5)
-  plt.plot(xx[0], yy[0], '.r',xx[-1], yy[-1], '.r', markersize=12)
-  plt.text(2,13,'b0', fontsize=13)
-  plt.text(0.05,10,'a0', fontsize=13)
-  plt.axvline(color='black'), plt.axhline(color='black')
-  plt.pause(2)
-  
  
   rng = 0.01
   a, b = golden_method(f, xx, rng)
 
-  t = np.logical_and(xx > a , xx < b)
-  xt = xx[t]
-  yt = yy[t]
 
-  plt.figure(figsize=(8, 6))
-  plt.plot(xx, yy, linewidth=2.5)
-  plt.axvline(color='black'), plt.axhline(color='black')
-  plt.plot(a, f.subs(x, a), '.r', b, f.subs(x, b), '.r', markersize=15)
-  plt.text(0.1, 10,  f'b - a = { np.round(b-a, 3)} < {rng}',fontsize=25)
 
  
